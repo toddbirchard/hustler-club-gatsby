02a3c4ea29de386538394302e4249a9f7314e41f
Todd Birchard <toddbirchard@gmail.com>
2019-11-15 23:24:51 -0500

    now serving New Yorks highest calibre gentlemen

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,15 +4,21 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (<Layout>
+const IndexPage = () => (
+  <Layout>
   <SEO title="Home"/>
   <div className="intro">
     <h1>{`HUSTLERS`}</h1>
     <h2>{`New York's finest Gentleman Club`}</h2>
     <h3>{`A Broiest Production. Invitation only.`}</h3>
-  <video class="videoObject center" autoplay loop autobuffer muted playsinline id="myVideo">
-    <source src="https://storage.cloud.google.com/planetjupyter/hustlers.mov" type="video/mp4" /></video>
-  </div>
-</Layout>)
+    </div>
+    <div className="video-container">
+      <video className="videoObject center" autoPlay loop muted playsInline id="myVideo">
+        <source src="vid/background.mp4" type="video/mp4" />
+      </video>
+    </div>
+
+</Layout>
+)
 
 export default IndexPage
